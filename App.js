9ee002f4e474124308f8fb51aba866a78e9ca360
“Dmitriy <loseex@vk.com>
2023-02-16 09:56:12 +0500

    clear

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { FButton } from './src/components/Buttons/button'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Это страница теста компонентов.</Text>
-      <FButton title="Click"/>
-      <FButton title="123"/>
       <StatusBar style="auto" />
     </View>
   );
