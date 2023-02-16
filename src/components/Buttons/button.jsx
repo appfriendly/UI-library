9f92b57ba035cd7663e42a760ec1bece9c277bc3
“Dmitriy <loseex@vk.com>
2023-02-16 10:18:51 +0500

    default button preset is ready

--- src/components/Buttons/button.jsx
+++ src/components/Buttons/button.jsx
@@ -1,16 +1,30 @@
 import * as React from 'react'
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableHighlight } from "react-native";
 
 export const FButton = (props) => {
+    const [isPress, setIsPress] = React.useState(false);
+
+    const touchProps = {
+        activeOpacity: 1,
+        underlayColor: '#252525',                               // <-- "backgroundColor" will be always overwritten by "underlayColor"
+        style: isPress ? styles.btnPress : styles.btnNormal, // <-- but you can still apply other style changes
+        onHideUnderlay: () => setIsPress(false),
+        onShowUnderlay: () => setIsPress(true),
+        onPress: () => console.log('user click on btn'),                 // <-- "onPress" is apparently required
+    };
     return (
-        <View style={styles.background}>
+        <TouchableHighlight {...touchProps}>
             <Text style={styles.title}>{props.title}</Text>
-        </View>
+        </TouchableHighlight>
     )
 }
 
 const styles = StyleSheet.create({
-    background: {
+    title: {
+        color: '#fff',
+        fontSize: 28,
+    },
+    btnNormal: {
         flex: '0',
         justifyContent: 'center',
         alignItems: 'center',
@@ -19,13 +33,20 @@ const styles = StyleSheet.create({
         paddingHorizontal: 'auto',
         paddingVertical: 14,
         borderRadius: '20',
+        marginBottom: 20,
+      },
+    btnPress: {
+        flex: '0',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '90%',
+        backgroundColor: '#252525',
+        paddingHorizontal: 'auto',
+        paddingVertical: 14,
+        borderRadius: '20',
         marginBottom: 20
-    },
-    title: {
-        color: '#fff',
-        fontSize: 28,
-    },
-  });
+    }
+});
 
 
 
